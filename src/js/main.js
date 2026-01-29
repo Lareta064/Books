087188df6536772de4b0cec5d6d6543388cd8f7d
Lareta064 <lareta1@yandex.ru>
2026-01-29 17:19:53 +0400

    fix

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+	 /*MENU MOBILE */
+    const  bodyEl = document.body;
+	 const menuButton = document.querySelector('#menu-toggle');
+    const mobileMenu = document.querySelector('#header-menu');
+    
+      function closeMobileMenu(){
+        menuButton.classList.remove('active');
+        mobileMenu.style.height = 0;
+        bodyEl.classList.remove('lock');
+      }
+      function openMobileMenu(){
+        menuButton.classList.add('active');
+         mobileMenu.style.height =  mobileMenu.scrollHeight + 'px';
+        bodyEl.classList.add('lock');
+      }
+        menuButton.addEventListener('click', ()=> {
+      
+          if( menuButton.classList.contains('active')){
+             closeMobileMenu();
+            
+          }else{
+             openMobileMenu();
+			 console.log('555');
+          }
+      });
+})
